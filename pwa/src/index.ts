7ec1fff980d91830169e6d0d31903b4c203010b5
Pavel Sobolev <contact@paveloom.dev>
2024-10-12 13:24:41 +0300

    PWA: don't wait for the `load` event to fire before registering a SW.
    For some reason, both Chrome and Firefox stopped sending it. Vivaldi
    works fine. Seems like the service worker script is downloaded anyway.

--- pwa/src/index.ts
+++ pwa/src/index.ts
@@ -8,12 +8,10 @@ if ("storage" in navigator && "persist" in navigator.storage) {
 }
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then((registration) => {
-      console.log("SW registered: ", registration);
-    }).catch((registrationError: unknown) => {
-      console.log("SW registration failed: ", registrationError);
-    });
+  navigator.serviceWorker.register("service-worker.js").then((registration) => {
+    console.log("SW registered: ", registration);
+  }).catch((registrationError: unknown) => {
+    console.log("SW registration failed: ", registrationError);
   });
 }
 
