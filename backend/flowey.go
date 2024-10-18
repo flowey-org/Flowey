81603cb997494fda328c5aedb991ec1609736e26
Pavel Sobolev <contact@paveloom.dev>
2024-10-18 12:42:22 +0300

    Backend: allow overriding the IP and the port.

--- backend/flowey.go
+++ backend/flowey.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 )
 
-func run() error {
-	address := "0.0.0.0:80"
+func run(address string) error {
 	server := NewServer(address)
 	return server.ListenAndServe()
 }
 
 func main() {
-	if err := run(); err != nil {
+	ip := flag.String("ip", "0.0.0.0", "ip to bind to")
+	port := flag.Int("port", 80, "port to bind to")
+	flag.Parse()
+
+	address := fmt.Sprintf("%s:%d", *ip, *port)
+	if err := run(address); err != nil {
 		log.Fatal(err)
 	}
 }
