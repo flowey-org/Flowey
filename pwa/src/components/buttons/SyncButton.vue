aba3ae20737b445ddb06aeb0eb288eefee236014
Pavel Sobolev <contact@paveloom.dev>
2024-11-01 22:31:42 +0300

    PWA: don't use the `URL` class.
    Because it can throw an exception from the constructor.

--- pwa/src/components/buttons/SyncButton.vue
+++ pwa/src/components/buttons/SyncButton.vue
@@ -41,7 +41,7 @@ async function handleSubmit() {
 
   let response: Response;
   try {
-    response = await fetch(new URL("session", state.endpoint.value), {
+    response = await fetch(state.endpoint.value + "session/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -90,7 +90,7 @@ async function handleLogout() {
 
   let response: Response;
   try {
-    response = await fetch(new URL("session", state.endpoint.value), {
+    response = await fetch(state.endpoint.value + "session/", {
       method: "DELETE",
       credentials: "include",
     });
